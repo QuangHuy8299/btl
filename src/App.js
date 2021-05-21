41c0d05255313898fe0ece2373a2363077275825
Huy Pham <huy.pham@sapotacorp.vn>
2021-05-21 10:33:40 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './default.scss';
 import Header from './components/Header';
-import Homepage from './pages/Homepage'
+import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <Header />
       <div className="main">
         <Homepage />
+        <Registration />
       </div>
+      <Footer/>
     </div>
   );
 }
