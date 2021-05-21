c266a4d7b6d2240f2309f3ea651cc953f154b024
Huy Pham <huy.pham>
2021-05-21 15:37:32 +0700

    login Google

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles.scss';
 import Logo from './../../Asset/logo.jpg';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { auth } from './../../firebase/utils'
 
 const Header = props => {
+  const { currentUser } = props;
   return (
     <header className="header">
       <div className="warp">
@@ -13,17 +15,38 @@ const Header = props => {
           </Link>
         </div>
         <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">
-                Registration
+          {currentUser && (
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>
+                  LogOut
+              </span>
+              </li>
+            </ul>
+          )}
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">
+                  Register
               </Link>
-            </li>
-          </ul>
+              </li>
+              <li>
+                <Link to="/login">
+                  Login
+              </Link>
+              </li>
+            </ul>
+          )}
+
         </div>
       </div>
     </header>
   )
+}
+
+Header.defaultProps = {
+  currentUser: null
 }
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-0omqatUmv3EBLGigvEKDavyCW6Jsj_U",
+  authDomain: "ecommrecewebsite.firebaseapp.com",
+  projectId: "ecommrecewebsite",
+  storageBucket: "ecommrecewebsite.appspot.com",
+  messagingSenderId: "469403940578",
+  appId: "1:469403940578:web:691c58b8ca1dc4d25910bb"
+}
